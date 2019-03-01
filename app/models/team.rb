35a0f31fd99e666e978d31d532c690666cf00a63
Loganarmack <loganarmack@gmail.com>
2019-02-28 21:50:10 -0500

    Added extra data to teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -9,4 +9,15 @@ class Team < ApplicationRecord
   attribute :driver_skill, :integer
   attribute :autonomous, :integer
   attribute :issues, :string
+  attribute :defence, :integer
+  attribute :move_cargo, :boolean
+  attribute :cargo_max, :integer
+  attribute :cargo_pickup, :integer
+  attribute :move_hatch, :boolean
+  attribute :hatch_max :integer
+  attribute :hatch_pickup, :integer
+  attribute :climb_level, :integer
+  attribute :climb_notes, :string
+  attribute :sandstorm_mode, :integer
+  attribute :sandstorm_notes, :string
 end
